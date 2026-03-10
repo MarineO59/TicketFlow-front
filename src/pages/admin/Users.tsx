d058f8057891249396120e2e41ac826bd656c5f7
Victor dS <victordespirlet@gmail.com>
2026-03-10 16:13:26 +0100

    feat: ajout de la page Users admin

---
+++ src/pages/admin/Users.tsx
@@ -0,0 +1,97 @@
+import {
+	Box,
+	Paper,
+	Table,
+	TableBody,
+	TableCell,
+	TableContainer,
+	TableHead,
+	TableRow,
+	Typography,
+} from "@mui/material";
+import { useEffect, useState } from "react";
+import Profile from "./ProfileUser";
+import User from "./UserCard";
+
+export default function Users() {
+	const [users, setUsers] = useState([]);
+	const [currentUser, setCurrentUser] = useState(null);
+	const [isUpdate, SetIsUpdate] = useState(false);
+	console.log(currentUser);
+
+	useEffect(() => {
+		fetch("http://localhost:3310/api/users/")
+			.then((response) => response.json())
+			.then((data) => setUsers(data))
+			.catch((error) => console.error(error));
+	}, [isUpdate]);
+
+	return (
+		<Box sx={{ p: 3 }}>
+			<Typography variant="h4" gutterBottom>
+				Gestion des utilisateurs
+			</Typography>
+			<TableContainer component={Paper}>
+				<Table>
+					<TableHead>
+						<TableRow>
+							<TableCell
+								sx={{
+									fontWeight: "bold",
+									bgcolor: "primary.main",
+									color: "white",
+								}}
+							>
+								Prénom
+							</TableCell>
+							<TableCell
+								sx={{
+									fontWeight: "bold",
+									bgcolor: "primary.main",
+									color: "white",
+								}}
+							>
+								Nom
+							</TableCell>
+							<TableCell
+								sx={{
+									fontWeight: "bold",
+									bgcolor: "primary.main",
+									color: "white",
+								}}
+							>
+								Email
+							</TableCell>
+							<TableCell
+								sx={{
+									fontWeight: "bold",
+									bgcolor: "primary.main",
+									color: "white",
+								}}
+							>
+								Modifier
+							</TableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{users.length > 0 ? (
+							users.map((user) => (
+								<User
+									key={user.id}
+									user={user}
+									setCurrentUser={setCurrentUser}
+									SetIsUpdate={SetIsUpdate}
+								/>
+							))
+						) : (
+							<TableRow>
+								<TableCell colSpan={4}>No Data</TableCell>
+							</TableRow>
+						)}
+					</TableBody>
+				</Table>
+			</TableContainer>
+			{currentUser && <Profile currentUser={currentUser} />}
+		</Box>
+	);
+}
