2439addb0506e5be3c744ce3893730820f8ca19a
Victor dS <victordespirlet@gmail.com>
2026-04-09 18:16:54 +0200

    feat: ling fixing bugs

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,16 +1,16 @@
 export const fetchWithToken = (url: string, options: RequestInit = {}) => {
-  const token = localStorage.getItem("token");
+	const token = localStorage.getItem("token");
 
-  const isFormData = options.body instanceof FormData;
+	const isFormData = options.body instanceof FormData;
 
-  const headers: HeadersInit = {
-    ...(isFormData ? {} : { "Content-Type": "application/json" }),
-    Authorization: `Bearer ${token}`,
-    ...options.headers,
-  };
+	const headers: HeadersInit = {
+		...(isFormData ? {} : { "Content-Type": "application/json" }),
+		Authorization: `Bearer ${token}`,
+		...options.headers,
+	};
 
-  return fetch(url, {
-    ...options,
-    headers,
-  });
+	return fetch(url, {
+		...options,
+		headers,
+	});
 };
